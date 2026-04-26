933d3886d29d6bb90a9f79f36845e696296352c6
Vovchik Gorobchik <ivanzol02server@gmail.com>
2026-04-26 16:15:47 +0300

    Delete shemas directory

--- shemas/review_schema.py
+++
@@ -1,9 +0,0 @@
-
-from ma import ma
-from models.reviews.reviews import Reviews
-
-class ReviewSchema(ma.SQLAlchemyAutoSchema):
-    class Meta:
-        model = Reviews
-        include_fk = True
-        load_instance = True
