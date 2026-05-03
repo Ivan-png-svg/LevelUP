7d857b0796d41394f1840e394a667f418b07dc9a
Vovchik Gorobchik <ivanzol02server@gmail.com>
2026-05-03 03:52:34 +0300

    Add files via upload

---
+++ scr/app/pages/Home.tsx
@@ -0,0 +1,15 @@
+import { Hero } from '../components/Hero';
+import { Services } from '../components/Services';
+import { WhyChooseUs } from '../components/WhyChooseUs';
+import { Testimonials } from '../components/Testimonials';
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Services />
+      <WhyChooseUs />
+      <Testimonials />
+    </>
+  );
+}
